8b6ea4137ebbf4a075c115bdc6e063d676c7a712
Alvaro Navarro <38303261+alvaroanavarro1@users.noreply.github.com>
2021-02-09 02:02:09 -0400

    First script try
    primer intento por página web, la próxima ves se intentara por libro

---
+++ AXL/axl_api_test.py
@@ -0,0 +1,39 @@
+from zeep import Client
+from zeep.cache import SqliteCache
+from zeep.transports import Transport
+from zeep.exceptions import Fault
+from zeep.plugins import HistoryPlugin
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
+from lxml import etree
+
+disable_warnings(InsecureRequestWarning)
+username = 'administrator'
+password = 'ccmcaf2015'
+host = '10.200.160.30'
+
+wsdl = 'C:/Users/anavarro/OneDrive - SOUTEC/Desktop/Soutec/Cisco-UC-automations/AXL/schema/current/AXLAPI.wsdl'
+location = 'https://{host}:8443/axl/'.format(host=host)
+binding = "{http://www.cisco.com/AXLAPIService/}AXLAPIBinding"
+
+session = Session()
+session.verify = False
+session.auth = HTTPBasicAuth(username, password)
+
+transport = Transport(cache=SqliteCache(), session=session, timeout=20)
+history = HistoryPlugin()
+client = Client(wsdl=wsdl, transport=transport, plugins=[history])
+service = client.create_service(binding, location)
+
+def show_history():
+    for item in [history.last_sent, history.last_received]:
+        print(etree.tostring(item["envelope"], encoding="unicode", pretty_print=True))
+
+try:
+    resp = service.listPhone(searchCriteria={'name': 'SEP'}, 
+                             returnedTags={'name': '', 'description': ''})
+    print(resp)
+except Fault:
+    show_history()
